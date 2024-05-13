e19e09e4556f77b50a0254ff55558f7f93a5251d
samhsiao6238 <samhsiao6238@gmail.com>
2024-05-13 21:11:09 +0800

    s111

--- 01_數據類型/example.py
+++ 01_數據類型/example.py
@@ -1,6 +1,7 @@
 """
 這個腳本是提供給 mypy 測試使用
 """
+
 from typing import List
 
 
@@ -15,5 +16,5 @@ print("Sum:", result)
 
 # 包含類型錯誤的調用
 # 會引起 MyPy 檢查錯誤，因為傳入的是字串而非整數列表
-result2 = list_sum("hello")  
+result2 = list_sum("hello")
 print("This call is incorrect:", result2)
